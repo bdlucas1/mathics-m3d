51efc6720579c796a8ae47fafc32130a8f133227
Bruce Lucas <bdlucas1@users.noreply.github.com>
2025-12-02 17:11:09 -0500

    use util.Browser

--- view.py
+++ view.py
@@ -5,8 +5,9 @@ import subprocess
 import sys
 import time
 import webbrowser
-
 import webview
+
+import util
 
 parser = argparse.ArgumentParser(description="Graphics demo")
 parser.add_argument("url", type=str)
@@ -18,10 +19,14 @@ args = parser.parse_args()
 process = subprocess.Popen(args.cmd, stdout=sys.stdout, stderr=sys.stderr)
 time.sleep(args.delay)
 
+"""
 if args.browser == "webview":
     webview.create_window(args.url, args.url, x=50, y=50, width=900, height=1200)
     webview.start()
 elif args.browser == "webbrowser":
     webbrowser.open_new(args.url)
     process.wait()
-    
+"""    
+
+util.Browser().show(args.url).start()
+time.sleep(1e6)
